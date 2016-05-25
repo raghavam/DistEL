714e998754f6ac2182801aa617ef3b14fe798ffd
raghavam <m.vijayaraghava@gmail.com>
2016-05-25 12:34:58 -0400

    updated the directory structure

--- scripts/classifier.sh
+++ scripts/classifier.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd DistributedReasoning/curr_approach
+cd DistEL/
 echo -e "\nStarting ELClassifier....\n"
 java -Xms12g -Xmx12g -cp dist/DistEL.jar:lib/jedis-2.8.1.jar knoelab.classification.ELClassifier
