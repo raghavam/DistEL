d10ec97533ccf330f32f1c6cdb1c11507eca8608
raghavam <m.vijayaraghava@gmail.com>
2016-05-26 16:03:54 -0400

    does not need any input

--- scripts/test-classify.sh
+++ scripts/test-classify.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-        echo -e "\nrequires Ontology file name"
-        exit 1
-fi
+#if [ $# -ne 1 ]; then
+#        echo -e "\nrequires Ontology file name"
+#        exit 1
+#fi
 
 echo -e "Checking the progress of classifier...\n"
 
-java -Xms6g -Xmx6g -cp dist/DistEL.jar:lib/jedis-2.6.2.jar knoelab.classification.test.ELClassifierTest $1
+java -Xms12g -Xmx12g -cp dist/DistEL.jar:lib/jedis-2.6.2.jar knoelab.classification.test.ELClassifierTest
