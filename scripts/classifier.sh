6eb448e669f15c293346103ef213116f3ec544ce
raghavam <m.vijayaraghava@gmail.com>
2016-05-27 09:53:45 -0400

    set 10gb heap size

--- scripts/classifier.sh
+++ scripts/classifier.sh
@@ -2,4 +2,4 @@
 
 cd DistEL/
 echo -e "\nStarting ELClassifier....\n"
-java -Xms12g -Xmx12g -cp dist/DistEL.jar:lib/jedis-2.8.1.jar knoelab.classification.ELClassifier
+java -Xms10g -Xmx10g -cp dist/DistEL.jar:lib/jedis-2.8.1.jar knoelab.classification.ELClassifier
