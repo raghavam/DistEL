c2da4302caab7e0e9de8ddde73abd9396ac15541
raghavam <m.vijayaraghava@gmail.com>
2016-06-01 02:33:17 -0400

    uncommented gcc install

--- scripts/install-redis.sh
+++ scripts/install-redis.sh
@@ -4,7 +4,7 @@ wget https://github.com/antirez/redis/archive/2.6.2.tar.gz
 mv 2.6.2.tar.gz redis-2.6.2.tar.gz
 tar -xvzf redis-2.6.2.tar.gz
 cd redis-2.6.2/
-#sudo apt-get install make
-#sudo apt-get --assume-yes install gcc
+sudo apt-get install make
+sudo apt-get --assume-yes install gcc
 make distclean
 make
